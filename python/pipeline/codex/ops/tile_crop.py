340feef9ca1c0c0662206b49b8b8064372b00db4
eric-czech <eric.allen.czech@gmail.com>
2018-04-28 04:41:48 -0700

    Adding tiling abstractions

--- python/pipeline/codex/ops/tile_crop.py
+++ python/pipeline/codex/ops/tile_crop.py
@@ -9,17 +9,27 @@ logger = logging.getLogger(__name__)
 # - tile overlap x = 576, tile overlap y = 432 (x refers to columns and y refers to rows)
 
 
+def get_slice(config):
+    """Get 2D slice defining crop operation appropriate for given configuration"""
+    nw, nh = config.tile_width, config.tile_height
+    ow, oh = config.overlap_x, config.overlap_y
+    w_start, h_start = ow // 2, oh // 2
+    w_stop, h_stop = w_start + nw, h_start + nh
+    return [slice(h_start, h_stop), slice(w_start, w_stop)]
+
+
+def apply_slice(img, crop_slice):
+    """Apply cropping slice to trailing image dimensions"""
+    if img.ndim < 2:
+        raise ValueError('Expecting at least 2 dimensions in image (shape of given array = {})'.format(img.shape))
+    slices = [slice(None, None) for _ in range(img.ndim - 2)] + crop_slice
+    return img[slices]
+
+
 class CodexTileCrop(CodexOp):
 
     def __init__(self, config):
         super(CodexTileCrop, self).__init__(config)
 
     def run(self, tile):
-        nw, nh = self.config.tile_width, self.config.tile_height
-
-        ow, oh = self.config.overlap_x, self.config.overlap_y
-        w_start, h_start = ow // 2, oh // 2
-        w_stop, h_stop = w_start + nw, h_start + nh
-
-        # Tile should have shape (cycles, z, channel, height, width)
-        return tile[:, :, :, h_start:h_stop, w_start:w_stop]
+        return apply_slice(tile, get_slice(self.config))

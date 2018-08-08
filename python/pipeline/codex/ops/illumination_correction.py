48cb87ba1dbd6e05dc32a26820475b9de7c9eba1
eric-czech <eric.allen.czech@gmail.com>
2018-08-07 21:17:34 -0400

    Adding tile and region illumination correction fetures

--- python/pipeline/codex/ops/illumination_correction.py
+++ python/pipeline/codex/ops/illumination_correction.py
@@ -9,26 +9,24 @@ from codex.function import data as function_data
 from sklearn.linear_model import HuberRegressor, Ridge, LinearRegression
 from sklearn.neighbors import KNeighborsRegressor
 from sklearn.ensemble import GradientBoostingRegressor
+from sklearn.neural_network import MLPRegressor
+from sklearn.preprocessing import PolynomialFeatures, StandardScaler
+from sklearn.pipeline import Pipeline
 from skimage import transform
 import logging
 logger = logging.getLogger(__name__)
 
-# filter_params:
-# percentile_range: [.01, .99]
-# features: [DAPI, cell_size]
-# max_cells: 250000
-# feature_params: {tile: {degree: 2}, region: {degree: 2}}
-
 DEFAULT_FILTER_PARAMS = dict(percentile_range=[.01, .99], features=['cell_size'], max_cells=250000)
-DEFAULT_FEATURE_PARAMS = dict(tile=dict(degree=2), region=dict(degree=2))
+DEFAULT_FEATURE_PARAMS = dict(tile=dict(type='polynomial', degree=2), region=dict(type='polynomial', degree=2))
 DEFAULT_MODEL_PARAMS = dict(type='ls')
 DEFAULT_PREDICTION_PARAMS = dict(step_size=10)
 MODELS = {
-    'ls': {'factory': lambda args: LinearRegression(**args)},
+    'ls': {'factory': lambda args: LinearRegression(**{**dict(normalize=True), **args})},
     'huber': {'factory': lambda args: HuberRegressor(**args)},
-    'ridge': {'factory': lambda args: Ridge(**args)},
+    'ridge': {'factory': lambda args: Ridge(**{**dict(normalize=True), **args})},
     'gbr': {'factory': lambda args: GradientBoostingRegressor(random_state=SEED, **args)},
-    'knn': {'factory': lambda args: KNeighborsRegressor(**args)}
+    'knn': {'factory': lambda args: KNeighborsRegressor(**args)},
+    'mlp': {'factory': lambda args: Pipeline([('scale', StandardScaler()), ('est', MLPRegressor(**args))])}
 }
 SEED = 5512
 
@@ -45,16 +43,21 @@ def _get_params(key, params, defaults):
     return res
 
 
-def get_prediction_features(tile_shape, region_shape, step=1):
-    step_size = self.prediction_params['step_size']
-    r, c = shape
-    ri, ci = np.arange(0, r, step_size), np.arange(0, c, step_size)
+def _get_coordinate_features(region_shape, tile_shape, step=1):
+    ri, ci = np.arange(0, region_shape[0], step), np.arange(0, region_shape[1], step)
+    nr, nc = len(ri), len(ci)
 
     # Produce 2D array with 2 cols, ry and rx
-    X_region = np.transpose([np.repeat(ri, len(ci)), np.tile(ci, len(ri))])
-    X_tile = np.mod()
+    X_region = np.transpose([np.repeat(ri, nc), np.tile(ci, nr)])
 
-    pass
+    # Mod region coordinates by tile height and width
+    X_tile = np.mod(X_region, tile_shape)
+
+    # Return all 4 features as a data frame as well as number of (potentially sampled
+    # coordinate points along each dimension)
+    X = pd.DataFrame(np.hstack((X_region, X_tile)), columns=['ry', 'rx', 'y', 'x'])
+    return X, (nr, nc)
+
 
 class IlluminationCorrection(codex_op.CodexOp):
 
@@ -73,13 +76,20 @@ class IlluminationCorrection(codex_op.CodexOp):
         self.channel_mapping = pd.Series(params['channel_mapping']).sort_index()
 
         # Extra parameters relevant to each step
-        self.filter_params = _get_params('filter_params', filter_params)
-        self.feature_params = _get_params('feature_params', feature_params)
-        self.model_params = _get_params('model_params', model_params)
-        self.prediction_params = _get_params('prediction_params', prediction_params)
+        self.filter_params = _get_params('filter_params', params, filter_params)
+        self.feature_params = _get_params('feature_params', params, feature_params)
+        self.model_params = _get_params('model_params', params, model_params)
+        self.prediction_params = _get_params('prediction_params', params, prediction_params)
 
         # Extract flag indicating whether or not to overwrite original tiles with corrected results
         self.overwrite_tile = params.get('overwrite_tile', overwrite_tile)
+
+        # Extra commonly used shape information from experiment configuration
+        self.region_shape = (
+            self.config.region_height * self.config.tile_height,
+            self.config.region_width * self.config.tile_width
+        )
+        self.tile_shape = self.config.tile_height, self.config.tile_width
 
         ############################
         # Configuration Validation #
@@ -104,8 +114,8 @@ class IlluminationCorrection(codex_op.CodexOp):
             )
         if self.model_params['type'] not in MODELS:
             raise ValueError(
-                '`type` in model_params type must be one of [] (given = "{}")'
-                .format(self.model_params['type'], list(MODELS.keys()))
+                '`type` in model_params type must be one of {} (given = "{}")'
+                .format(list(MODELS.keys()), self.model_params['type'])
             )
 
         self.data = None
@@ -146,22 +156,30 @@ class IlluminationCorrection(codex_op.CodexOp):
                 feats.append(ch_feat)
                 continue
             raise ValueError('Feature "{}" not present in cytometry data'.format(feat))
-
-
-    def _get_prediction_features(self):
-
-
-        step_size = self.prediction_params['step_size']
-        r, c = shape
-        ri, ci = np.arange(0, r, step_size), np.arange(0, c, step_size)
-
-        # Produce 2D array with 2 cols, ry and rx
-        X = np.transpose([np.repeat(ri, len(ci)), np.tile(ci, len(ri))])
-
-        pass
+        return feats
 
     def _prepare_prediction_features(self, df):
-        pass
+
+        # Feature grep preparation function
+        def prep(X, params):
+            type = params.get('type')
+            if not type or type == 'none':
+                return None
+            if type != 'polynomial':
+                raise ValueError(
+                    'Feature preparation type "{}" is not valid (must be None or "polynomial")'.format(type))
+            return PolynomialFeatures(degree=params['degree'], include_bias=False).fit_transform(X)
+
+        # Get features for each group (region and tile) or None, if either is disabled
+        features = [x for x in [
+            prep(df[['ry', 'rx']], self.feature_params['region']),
+            prep(df[['y', 'x']], self.feature_params['tile'])
+        ] if x is not None]
+        if len(features) == 0:
+            raise ValueError('At least one of region or tile features must be enabled')
+
+        # Concatenate features, if necessary
+        return features[0] if len(features) == 1 else np.hstack(tuple(features))
 
     def _estimate_model(self, X, y):
         # Fit regression model used to represent illumination surface
@@ -188,13 +206,16 @@ class IlluminationCorrection(codex_op.CodexOp):
                 raise ValueError(
                     'Cytometry data empty after application of feature filters for channel {}'.format(channel))
 
+            # Extract spatial cell features and prediction target
+            X = self._prepare_prediction_features(dfm[['ry', 'rx', 'y', 'x']])
+            y = dfm[DEFAULT_CHANNEL_PREFIX + channel]
+
             logger.debug(
-                'Building illumination model for region %s, channel "%s" using %s cells (%s originally)',
-                region_index, channel, len(dfm), n
+                'Building illumination model for region %s, channel "%s" using %s cells (%s originally) [feature '
+                'array shape = %s, response shape = %s]',
+                region_index, channel, len(dfm), n, X.shape, y.shape
             )
 
-            # Extract spatial cell features and prediction target
-            X, y = dfm[['ry', 'rx']], dfm[DEFAULT_CHANNEL_PREFIX + channel]
             if np.isclose(y.mean(), 0):
                 raise ValueError(
                     'Average {} channel intensity for region {} (across {} cells) is ~0, '
@@ -207,28 +228,28 @@ class IlluminationCorrection(codex_op.CodexOp):
             ests[channel] = self._estimate_model(X, y)
         return ests
 
-    def _estimate_image(self, est, shape):
+    def _estimate_image(self, est):
+        # Get prepared coordinate features for entire region and run predictions
         step_size = self.prediction_params['step_size']
-        r, c = shape
-        ri, ci = np.arange(0, r, step_size), np.arange(0, c, step_size)
-
-        # Produce 2D array with 2 cols, ry and rx
-        X = np.transpose([np.repeat(ri, len(ci)), np.tile(ci, len(ri))])
-
-        y = est.predict(X)
+        df, image_shape = _get_coordinate_features(self.region_shape, self.tile_shape, step_size)
+        y = est.predict(self._prepare_prediction_features(df))
 
         # Some models may intentionally produce nans for pixels too far away from foreground
         # objects so set those values to 1 as this will lead to no change in correction
         y = np.where(np.isnan(y), 1., y)
 
-        # Reshape to image shape
-        y = y.reshape((len(ri), len(ci)))
+        # Reshape from 1D to 2D image shape (which will be shape of entire region or a downsampled version of it)
+        y = y.reshape(image_shape)
 
         # Upsample if necessary
         if step_size > 1:
-            y = transform.resize(y, shape, mode='constant', order=1, anti_aliasing=False, preserve_range=False)
+            y = transform.resize(
+                y, self.region_shape, mode='constant', order=1,
+                anti_aliasing=False, preserve_range=False
+            )
 
-        assert y.shape == shape, 'Expecting result with shape {} but was {}'.format(shape, y.shape)
+        assert y.shape == self.region_shape, \
+            'Expecting result with shape {} but was {}'.format(self.region_shape, y.shape)
         return y.astype(np.float32)
 
     def get_illumination_images(self, ests):
@@ -240,15 +261,9 @@ class IlluminationCorrection(codex_op.CodexOp):
             Dictionary mapping source channels to a 3D float32 array with shape equal to
             (region height * tile height, region width * tile width)
         """
-        # Get whole region shape as rows, cols
-        r, c = (
-            self.config.region_height * self.config.tile_height,
-            self.config.region_width * self.config.tile_width
-        )
-
         imgs = OrderedDict()
         for channel, est in ests.items():
-            imgs[channel] = self._estimate_image(est, (r, c))
+            imgs[channel] = self._estimate_image(est)
 
         if len(imgs) > 0:
             img = list(imgs.values())[0]

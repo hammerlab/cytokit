d7f09f0f46a88f54c4126c0695b0eb5e910abe54
eric-czech <eric.allen.czech@gmail.com>
2018-11-13 11:44:31 -0500

    Adding cli for starting applications

---
+++ python/pipeline/cytokit/cli/application.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python
+"""Application launcher CLI"""
+import fire
+import logging
+
+
+class Application(object):
+
+    def run_explorer(self):
+        logging.info('Running explorer app')
+        from cytokit_app.explorer import app
+        app.run()
+
+
+if __name__ == '__main__':
+    fire.Fire(Application)

d6a8dbf4ebaa8869c3a1fe833fe6612e44aedf58
eric-czech <eric.allen.czech@gmail.com>
2018-12-13 16:10:44 -0500

    Refactor cell size analysis

--- pub/analysis/cell-size/explorer_config.sh
+++ pub/analysis/cell-size/explorer_config.sh
@@ -1,11 +1,9 @@
 # source $CYTOKIT_REPO_DIR/pub/analysis/cell-size/explorer_config.sh; cytokit application run_explorer
 
-# Counting instrument:
-# https://www.thermofisher.com/us/en/home/life-science/cell-analysis/cell-analysis-instruments/automated-cell-counters/countess-ii-automated-cell-counter.html
-
 #export APP_EXP_NAME="20181024-d38-act-20X-5by5"
-export APP_EXP_NAME="20181024-d38-unstim-20X-5by5"
+#export APP_EXP_NAME="20181024-d38-unstim-20X-5by5"
 #export APP_EXP_NAME="20181024-jurkat-20X-5by5"
+export APP_EXP_NAME="20181024-jurkat2-20X-5by5"
 #export APP_EXP_NAME="20181026-pmel-act-60x-5b5"
 
 export APP_EXP_DATA_DIR=$CYTOKIT_DATA_DIR/cytokit/cell-size/20181024/$APP_EXP_NAME/output/v01
